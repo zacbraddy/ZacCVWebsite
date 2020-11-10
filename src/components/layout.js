6c57cf9157d91b6b034fc243fcd98ad3bcebdb76
Zac Braddy <zacharybraddy@gmail.com>
2020-11-10 14:21:53 +0000

    Make SEO component always render

--- src/components/layout.js
+++ src/components/layout.js
@@ -47,71 +47,74 @@ export default ({ children }) => {
     });
   }, []);
 
-  return loading ? (
+  return (
     <>
       <SEO />
-      <LoadingSpinner />
-    </>
-  ) : (
-    <>
-      <SEO />
-      <Theme />
-      <div className="lg:hidden">
-        <Menu
-          customBurgerIcon={
-            <FontAwesomeIcon
-              className="text-gray-100"
-              style={{ width: 'auto' }}
-              icon={faBars}
-            />
-          }
-          right
-          isOpen={menuOpen}
-          onStateChange={({ isOpen }) => setMenuOpen(isOpen)}
-        >
-          <NavLinks onClick={() => setMenuOpen(false)} />
-        </Menu>
-      </div>
-      <main className="p-2 h-screen">
-        <div className="h-full lg:flex lg:items-center font-sans xl:mx-auto">
-          <AnimatedContainer
-            className={`${container} transition h-full pt-4 lg:pt-0 lg:flex lg:flex-grow lg:mx-auto max-w-screen-lg xl:max-w-screen-xl`}
-          >
-            <div
-              className={`${hero} flex flex-col items-center rounded-l lg:grid lg:grid-rows-2 lg:pt-16 lg:gap-0 lg:flex-grow-0 lg:w-72 lg:bg-primary-200 lg:overflow-hidden`}
+      {loading ? (
+        <>
+          <LoadingSpinner />
+        </>
+      ) : (
+        <>
+          <Theme />
+          <div className="lg:hidden">
+            <Menu
+              customBurgerIcon={
+                <FontAwesomeIcon
+                  className="text-gray-100"
+                  style={{ width: 'auto' }}
+                  icon={faBars}
+                />
+              }
+              right
+              isOpen={menuOpen}
+              onStateChange={({ isOpen }) => setMenuOpen(isOpen)}
             >
-              <div className="grid grid-rows-2 gap-8 lg:mt-16 xl:mt-0">
-                <div className="w-68 flex justify-center">
-                  <PortraitImage />
-                </div>
-                <div className="hidden text-lg w-68 flex-col items-center lg:flex">
-                  <div>Zac Braddy</div>
-                  <div>Lead Software Engineer</div>
-                  <Socials />
-                </div>
-              </div>
-              <nav className="mt-8 xl:mt-0 justify-start flex-col h-full items-center hidden lg:flex">
-                <NavLinks />
-              </nav>
-            </div>
-            <div
-              className={`pt-16 mb-4 bg-primary-400 rounded h-full max-w-screen-md overflow-hidden sm:mb-2 md:pt-24 lg:flex-grow lg:pt-0 xl:max-w-screen-lg`}
-            >
-              <AnimateOnChange
-                className="h-full w-full"
-                animationIn="fadeInUp"
-                animationOut="bounceOut"
-                durationIn="100"
-                durationOut="100"
+              <NavLinks onClick={() => setMenuOpen(false)} />
+            </Menu>
+          </div>
+          <main className="p-2 h-screen">
+            <div className="h-full lg:flex lg:items-center font-sans xl:mx-auto">
+              <AnimatedContainer
+                className={`${container} transition h-full pt-4 lg:pt-0 lg:flex lg:flex-grow lg:mx-auto max-w-screen-lg xl:max-w-screen-xl`}
               >
-                <Scrollbar heightRelativeToParent="calc(100% - 20px)">
-                  {children}
-                </Scrollbar>
-              </AnimateOnChange>
+                <div
+                  className={`${hero} flex flex-col items-center rounded-l lg:grid lg:grid-rows-2 lg:pt-16 lg:gap-0 lg:flex-grow-0 lg:w-72 lg:bg-primary-200 lg:overflow-hidden`}
+                >
+                  <div className="grid grid-rows-2 gap-8 lg:mt-16 xl:mt-0">
+                    <div className="w-68 flex justify-center">
+                      <PortraitImage />
+                    </div>
+                    <div className="hidden text-lg w-68 flex-col items-center lg:flex">
+                      <div>Zac Braddy</div>
+                      <div>Lead Software Engineer</div>
+                      <Socials />
+                    </div>
+                  </div>
+                  <nav className="mt-8 xl:mt-0 justify-start flex-col h-full items-center hidden lg:flex">
+                    <NavLinks />
+                  </nav>
+                </div>
+                <div
+                  className={`pt-16 mb-4 bg-primary-400 rounded h-full max-w-screen-md overflow-hidden sm:mb-2 md:pt-24 lg:flex-grow lg:pt-0 xl:max-w-screen-lg`}
+                >
+                  <AnimateOnChange
+                    className="h-full w-full"
+                    animationIn="fadeInUp"
+                    animationOut="bounceOut"
+                    durationIn="100"
+                    durationOut="100"
+                  >
+                    <Scrollbar heightRelativeToParent="calc(100% - 20px)">
+                      {children}
+                    </Scrollbar>
+                  </AnimateOnChange>
+                </div>
+              </AnimatedContainer>
             </div>
-          </AnimatedContainer>
-        </div>
-      </main>
+          </main>
+        </>
+      )}
     </>
   );
 };
